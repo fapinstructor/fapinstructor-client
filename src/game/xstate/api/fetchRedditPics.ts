1dfc85a43f938f37978075d6420bb8bfe0c9af76
thefapinstructor <thefapinstructor@gmail.com>
2023-10-02 11:47:03 -0400

    make signed resolver async

--- src/game/xstate/api/fetchRedditPics.ts
+++ src/game/xstate/api/fetchRedditPics.ts
@@ -1,6 +1,6 @@
 import * as qs from "query-string";
 
-import { MediaRequest, MediaResponse } from "@/types/Media";
+import { MediaLink, MediaRequest, MediaResponse } from "@/types/Media";
 import { Severity } from "@/stores/notifications";
 import { createNotification } from "@/game/engine/notification";
 import { axios } from "@/lib/axios";
@@ -42,12 +42,33 @@ export default async function fetchRedditPics(request: MediaRequest) {
     });
   });
 
-  for (const link of res.links) {
-    if (!(link.mediaType.toLowerCase() === "video" && link.sourceLink && link.directLink.includes("redgifs"))) continue;
-    const i = res.links.indexOf(link);
-    const redGifID = link.directLink.split("/")[3].split("-")[0];
-    res.links[i] = await searchRedGifs(redGifID.toLowerCase());
-  }
+  const redGifLinks = await resolveRedgifsLinks(
+    res.links.filter((link) => link.directLink.includes("redgifs"))
+  );
+  const links = res.links.filter(
+    (link) => !link.directLink.includes("redgifs")
+  );
 
-  return res.links;
+  console.log(redGifLinks);
+
+  return [...links, ...redGifLinks];
+}
+
+// Asynchronously resolve signed urls for redgif links.
+async function resolveRedgifsLinks(redGifLinks: MediaLink[]) {
+  const redgifsSignedLinksPromise = await Promise.allSettled(
+    redGifLinks.map((link) => {
+      const redGifID = link.directLink.split("/")[3].split("-")[0];
+      return searchRedGifs(redGifID.toLowerCase());
+    })
+  );
+
+  const redgifsSignedLinks = redgifsSignedLinksPromise
+    .filter(
+      (linkPromise): linkPromise is { status: "fulfilled"; value: MediaLink } =>
+        linkPromise.status === "fulfilled"
+    )
+    .map((link) => link.value);
+
+  return redgifsSignedLinks;
 }
